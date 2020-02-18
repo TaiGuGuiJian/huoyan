ed691a9e0f71999e615916c8e72b0893deb85e14
TaiGuGuiJian <761155980@qq.com>
2020-02-18 20:00:48 +0800

    .gitignore

---
+++ src/main/java/com/sxdx/CrmApplication.java
@@ -0,0 +1,18 @@
+package com.sxdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author 李今朝
+ * @date 2020-02-05T19:30:58+08:00
+ */
+@SpringBootApplication
+public class CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
+
+}
